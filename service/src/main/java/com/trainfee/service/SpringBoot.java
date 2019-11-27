75d8db18221bb13f34effcb124ee0a4ff63f302c
Poowis <60070184@kmitl.ac.th>
2019-11-28 01:32:06 +0700

    add cross origin

--- service/src/main/java/com/trainfee/service/SpringBoot.java
+++ service/src/main/java/com/trainfee/service/SpringBoot.java
@@ -10,6 +10,7 @@ import org.json.simple.parser.ParseException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 @EnableAutoConfiguration
+@CrossOrigin
 public class SpringBoot {
 	private Reader rd = new Reader();
 	private Calculator cl = new Calculator();
